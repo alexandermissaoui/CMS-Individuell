afac2cfcd5baa66d78da40c018bc32528b84aef4
Alexander Missaoui <alexandermissaoui@gmail.com>
2023-05-29 23:51:31 +0200

    put/delete left

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -46,89 +46,19 @@ const ProductDetails = () => {
             <p>${product.price}</p>
           </div>
           <div className="addToCart">
-            {/* <button className='btn-minus'>-</button> */}
-            {/* <div className='count'><p>2</p></div> */}
-            {/* <button className='btn-plus'>+</button> */}
+           
             <button className='btn btn-cart'>UPDATE</button>
             <button className='btn btn-cart'>DELETE</button>
           </div>
-          <div className="product-color">
-            {/* <div className="btn red"></div> */}
-            {/* <div className="btn yellow"></div> */}
-            {/* <div className="btn lightblue"></div> */}
-            {/* <p>SKU:N/A</p> */}
-          </div>
-          <div className="wishlist">
-            {/* <button className='btn btn-wishlist'>Add to wishlist</button> */}
-          </div>
-          <div className="category">
-            {/* <p>Category: Table Lamp, Light</p> */}
-          </div>
+      
         </div>
-      </div>
-      {/* ____________________________________SECTION 2____________________________________ */}
-      <div className="productdetails-section-2">
-        <ul className='productdetails-section-2-navbar'>
-          {/* <li className='red'>DESCRIPTION</li>
-          <li>ADDITIONAL INFO</li> */}
-          {/* <li>REVIEWS &#40;0&#41;</li> */}
-          {/* <li>SHIPPING & DELIVERY</li> */}
-        </ul>
-        {/* <div className='container-product-description'>
 
-          <div className="container-name">
-            <h2 className='spacing'>{product.productName}</h2>
-          </div>
 
-          <div className="container-description-and-img">
-            <div className='container-description'>
-              <p className='spacing'>{product.description}</p>
-            </div>
-            <div className="container-img">
-              <img src={product.imgUrl} className='description-img' />
-            </div>
-          </div>
+      </div>
+            <div className="productdetails-section-2">
+       
+      </div>
 
-        </div> */}
-      </div>
-      {/* ____________________________________SECTION 3____________________________________ */}
-      <div className="productdetails-section-3">
-        {/* <h2>Related Products</h2>
-        <div className="product-container">
-          <div className="card">
-            <img src={product.imgUrl} className="img-fluid" />
-            <p>{product.productName.slice(0, 30)}...</p>
-            <span className='old-price'>${product.price}</span><span>${product.price}</span>
-          </div>
-          <div className="card">
-            <img src={product.imgUrl} className="img-fluid" />
-            <p>{product.productName.slice(0, 30)}...</p>
-            <span>${product.price}</span>
-          </div>
-          <div className="card">
-            <img src={product.imgUrl} className="img-fluid" />
-            <p>{product.productName.slice(0, 30)}...</p>
-            <span>${product.price}</span>
-          </div>
-          <div className="card">
-            <img src={product.imgUrl} className="img-fluid" />
-            <p>{product.productName.slice(0, 30)}...</p>
-            <span>${product.price}</span>
-          </div>
-        </div> */}
-      </div>
-      {/* ____________________________________SECTION 4____________________________________ */}
-      <div className='productdetails-section-4'>
-        <div className='product-container-sm'>
-          {/* <div className='card'>
-            <img src={product.imgUrl} className="img-fluid" />
-            <img src={product.imgUrl} className="img-fluid" />
-            <img src={product.imgUrl} className="img-fluid" />
-            <img src={product.imgUrl} className="img-fluid" />
-            <img src={product.imgUrl} className="img-fluid" />
-          </div> */}
-        </div>
-      </div>
     </div>
   )
 }
